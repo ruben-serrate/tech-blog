84f9fd67d095bc46ee5a005f7b9a44cba04f725c
Ruben Serrate <ruben.serrate@myunidays.com>
2016-04-22 17:08:23 +0100

    Remove unneeded step from readme + automatic host detection

--- readme.md
+++ readme.md
@@ -13,10 +13,4 @@ Wordpress as dependancy with composer
 
 ##Deployment process:
 1. In the repository's root folder, run composer install. Composer will create a wordpress folder with WP in it.
-2. Create a new "wp-config.php" inside the wordpress folder containing just these lines: ```
-<?php
-require(__DIR__.'/../wp-config.php');
-/*  actual wp-config lives in the repository's root folder, 
-    wordpress folder is not source controlled as it's managed via composer*/
-```
-3. Follow normal WP migration process: https://codex.wordpress.org/Moving_WordPress , keeping in mind that wp-config file that needs editing is in the root folder, not in /wordpress.
+2. Follow normal WP migration process: https://codex.wordpress.org/Moving_WordPress , keeping in mind that wp-config file that needs editing is in the root folder, not in /wordpress.
--- wp-config.php
+++ wp-config.php
@@ -46,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'Bm:|K|Z|d:>xy#fzVD2+6S{S6B/J#jlR^Jbftj;4<1)^W,%gas#`8CEf4M33lArJ');
-define('SECURE_AUTH_KEY',  't(aIE*+tR:O>78hAwv>?%-+Jo5wOT~o;,2%kZ$Or+).V099O~gF5m`6;B3q+*:vU');
-define('LOGGED_IN_KEY',    'uN=-_!. AV!z@H{;@)<!hi=Y|u4VQX[6ouw7Y/j(JEM~ut1-+i |7 [kJ98^Fims');
-define('NONCE_KEY',        '`,M%F_4OM/| 7V8btGH)Os{fX&t25RV3o1/`ybdT?(qaQ[Jqbxg>w$SGtQ,#hdNM');
-define('AUTH_SALT',        'KpNzKK|&`>uCgt39{t&m4)J~s$[I+jYa!)rHLm@D!<4iQv|s]LQKZ1)??2U$5!#t');
-define('SECURE_AUTH_SALT', '5mC%!tW%Xz.M, =zVS1*Nn)hhenx)vzy|tDOJlaAvFQ~,WM@)t0[b*U_|@FujWwO');
-define('LOGGED_IN_SALT',   '.W}%w<]gz9A/%lqR1%S)yg#M9R6=LK^D!s@+a`=8|OZ[553oKd6&:54uzrS4?s}P');
-define('NONCE_SALT',       'YmYuVQjlZNktj/neKmY@p$`1@#WhFvj)dKV#|!n<:A|.xcq&G({l{u:z#k[{CxMZ');
+define('AUTH_KEY',         '***');
+define('SECURE_AUTH_KEY',  '***');
+define('LOGGED_IN_KEY',    '***');
+define('NONCE_KEY',        '***');
+define('AUTH_SALT',        '***');
+define('SECURE_AUTH_SALT', '***');
+define('LOGGED_IN_SALT',   '***');
+define('NONCE_SALT',       '***');
 
 /**#@-*/
 
@@ -66,9 +66,16 @@ define('NONCE_SALT',       'YmYuVQjlZNktj/neKmY@p$`1@#WhFvj)dKV#|!n<:A|.xcq&G({l
 $table_prefix  = 'wp_';
 
 
+/*Change wp-content directory*/
+if(empty( $_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') {
+	$protocol="http://";
+} else {
+	$protocol="https://";
+}
 
-define( 'WP_CONTENT_DIR', __DIR__ . '/wp-content' );
-define( 'WP_CONTENT_URL', 'http://****/wp-content' );
+
+define( 'WP_CONTENT_DIR', __DIR__ . '/content' );
+define( 'WP_CONTENT_URL', $protocol.$_SERVER['SERVER_NAME']."/content" );
 
 /**
  * For developers: WordPress debugging mode.
